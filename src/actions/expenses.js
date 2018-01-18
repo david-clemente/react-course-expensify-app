8d9d050bb5d81e810a8d3ffad08e92138b8f1ad3
DSDQ082781\dclemente <david.clemente@eproseed.com>
2018-01-18 16:58:26 +0000

    setup startRemoveExpenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,6 +1,7 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
 
+//ADD_EXPENSE
 export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
     expense
@@ -25,23 +26,33 @@ export const startAddExpense = (expenseData = {}) => {
     }
 }
 
+//EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 })
 
+//REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        })
+    }
+}
+
+//SET_EXPENSE
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
     expenses
 })
 
-// export const startSetExpenses;
 export const startSetExpenses = () => {
     return (dispatch) => {
         return database.ref('expenses').once('value').then((snapshot) => {
